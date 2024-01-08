6191642a3006d7623c58147968852d5ddb958715
SaiHarsha502vvit <141471076+SaiHarsha502vvit@users.noreply.github.com>
2024-01-08 18:35:06 +0530

    added bcrypt and jwt in user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -98,6 +98,34 @@ userSchema.methods.isPasswordCorrect = async function(password)
     return await bcrypt.compare(password,this.password)
 }
 
+userSchema.methods.generateAccessToken= function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+            email:this.email,
+            username:this.username,
+            fullname:this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken= function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+           
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
 export default User = mongoose.model("User",userSchema)
 /* Here the Important Thing is we Export this thing 
    and We use this in other files Rather than Getting it Mongodb Database
