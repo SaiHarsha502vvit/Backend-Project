c63b50444f24e48a91feee150e7324f044e81198
SaiHarsha502vvit <141471076+SaiHarsha502vvit@users.noreply.github.com>
2024-01-08 10:31:31 +0530

    Some Commenting

--- Readme.md
+++ Readme.md
@@ -1,3 +1,15 @@
 This  is a Video Series on Backend with JavaScript 
 
 -[Model Link](https://app.eraser.io/workspace/YtPqZ1VogxGy1jzIDkzj)
+
+
+Why I used 
+
+user.model.js 
+video.model.js
+Employee.model.js
+
+
+Because In This way it Will be Easy to Identify What is What and 
+We can also use this type of convention in 
+Controllers aas well 
--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,3 +1,10 @@
+/*
+    Here we use this class to 
+    Send ApiResponse in a Particular Way 
+
+
+*/
+
 class ApiResponse {
     constructor(statusCode,data,message="Success"){
         this.statusCode=statusCode
@@ -7,12 +14,13 @@ class ApiResponse {
     }
 }
 
-// 1. Informational responses ( 100- 199)
-// 2. Successful responses ( 200- 299)
-// 3. Redirection messages ( 300- 399 )
-// 4. Client error responses ( 400- 499 )
-// 5. Server error responses ( 500 - 599 )
-    
-    
+
+/* 
+1. Informational responses ( 100- 199)
+2. Successful responses ( 200- 299)
+3. Redirection messages ( 300- 399 )
+4. Client error responses ( 400- 499 )
+5. Server error responses ( 500 - 599 ) 
+*/  
     
    
